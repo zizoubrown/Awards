95926d159b7a49a3d5a57c49feda3134bc097a9a
zizoubrown <zizumoha2015@gmail.com>
2020-10-27 17:47:44 +0300

    add models

--- rewards/models.py
+++ rewards/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=30)
+    image = models.ImageField(upload_to = 'project/')
+    description = models.CharField(max_length=150)
+    #link
+
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to = 'profile/')
+    user_bio = models.CharField(max_length=150)
+    project = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    #contact_info = 
