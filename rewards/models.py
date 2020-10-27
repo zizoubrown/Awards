733346e30be399ffb5a04e24bc50d47262ce09cf
zizoubrown <zizumoha2015@gmail.com>
2020-10-27 18:38:23 +0300

    modify models

--- rewards/models.py
+++ rewards/models.py
@@ -8,8 +8,15 @@ class Project(models.Model):
     description = models.CharField(max_length=150)
     #link
 
+    def __str__(self):
+        return self.title
+
 class Profile(models.Model):
     profile_pic = models.ImageField(upload_to = 'profile/')
     user_bio = models.CharField(max_length=150)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     #contact_info = 
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
