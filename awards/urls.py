b8e008fb63361023cd34ae10b304b356ae11ca4f
zizoubrown <zizumoha2015@gmail.com>
2020-10-27 18:43:10 +0300

    modify urls.py

--- awards/urls.py
+++ awards/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rewards/', include('rewards.urls')),
+    path('', include('rewards.urls')),
 ]
