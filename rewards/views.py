59386564ab3b1b249afe0bc8ebea0707b102dc77
zizoubrown <zizumoha2015@gmail.com>
2020-10-28 10:07:32 +0300

    add search.html

--- rewards/views.py
+++ rewards/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Profile, Project
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    post = Project.objects.all()
+
+    return render(request, 'index.html' ,{'post':post})
