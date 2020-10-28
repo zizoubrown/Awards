0186148888cb1e3d7e8d4b34201b3e77e7d91246
zizoubrown <zizumoha2015@gmail.com>
2020-10-28 10:33:00 +0300

    add rating model

--- rewards/models.py
+++ rewards/models.py
@@ -2,22 +2,85 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Project(models.Model):
-    title = models.CharField(max_length=30)
-    image = models.ImageField(upload_to = 'project/')
-    description = models.CharField(max_length=150)
-    link = models.URLField(max_length=150)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
 class Profile(models.Model):
-    profile_pic = models.ImageField(upload_to = 'profile/')
-    user_bio = models.CharField(max_length=150)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    #contact_info = 
-    user = models.OneToOneField(User,on_delete=models.CASCADE, related_name='profile')
-
+    profilePic = models.ImageField(upload_to='profile/',null=True)
+    bio = models.CharField(max_length=60,blank=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+    
+    def create_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+class Image(models.Model):
+    image = models.ImageField()
+    name = models.CharField(max_length=30)
+    caption = models.CharField(max_length = 60)
+    upload_date = models.DateTimeField(default=timezone.now)
+    # profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    likes = models.PositiveIntegerField(default=0)
+    link = models.CharField(max_length=100)
+
+    @classmethod
+    def search_by_name(cls,search_term):
+        jake = cls.objects.filter(name__icontains=search_term)
+        return jake
+
+    def no_of_ratings(self):
+        ratings = Rating.objects.filter(image=self)
+        return len(ratings)
+
+    def avg_rating(self):
+        sum = 0
+        ratings = Rating.objects.filter(image=self)
+        for rating in ratings:
+            sum += rating.stars
+        if len(ratings) > 0:
+            return ' '.join(['Average rating',str(sum / len(ratings))])
+        else:
+            return 'No ratings yet'
+
+    def __str__(self):
+        return self.caption
+    class Meta:
+        ordering = ['-upload_date']
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_caption(cls,id,caption):
+        captioned = Image.objects.filter(id=id).update(caption = caption)
+        return captioned
+
+    @classmethod
+    def get_all(cls):
+        imgs = Image.objects.all()
+        return imgs
+
+    @classmethod
+    def get_image_by_id(cls,id):
+        image = Image.objects.filter(id=Image.id)
+        return image
+
+class Rating(models.Model):
+  image = models.ForeignKey(Image, on_delete = models.CASCADE)
+  user = models.ForeignKey(User, on_delete = models.CASCADE)
+  stars = models.IntegerField(validators = [MinValueValidator(1),MaxValueValidator(5)])
