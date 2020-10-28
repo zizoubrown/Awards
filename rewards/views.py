fbe0b5973df9fd9d7254a2c8554fd5e1eb1141d0
zizoubrown <zizumoha2015@gmail.com>
2020-10-28 11:14:34 +0300

    add search view function

--- rewards/views.py
+++ rewards/views.py
@@ -22,4 +22,15 @@ def upload(request):
         form = PostPictureForm() 
     return render(request, 'upload.html', {'form' : form}) 
 
+def search_results(request):
 
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
