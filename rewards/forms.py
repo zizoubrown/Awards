0b9f97aaa4b17b0ffec0ee73807ca43114a18dd3
zizoubrown <zizumoha2015@gmail.com>
2020-10-28 15:46:36 +0300

    debug

--- rewards/forms.py
+++ rewards/forms.py
@@ -22,32 +22,11 @@ def UniqueUser(value):
 	if User.objects.filter(username__iexact=value).exists():
 		raise ValidationError('User with this username already exists.')
 
-class SignupForm(forms.ModelForm):
-	username = forms.CharField(widget=forms.TextInput(), max_length=30, required=True,)
-	email = forms.CharField(widget=forms.EmailInput(), max_length=100, required=True,)
-	password = forms.CharField(widget=forms.PasswordInput())
-	confirm_password = forms.CharField(widget=forms.PasswordInput(), required=True, label="Confirm your password.")
-
-	class Meta:
-
-		model = User
-		fields = ('username', 'email', 'password', 'confirm_password')
-
-	def __init__(self, *args, **kwargs):
-		super(SignupForm, self).__init__(*args, **kwargs)
-		self.fields['username'].validators.append(ForbiddenUsers)
-		self.fields['username'].validators.append(InvalidUser)
-		self.fields['username'].validators.append(UniqueUser)
-		self.fields['email'].validators.append(UniqueEmail)
-
-	def clean(self):
-		super(SignupForm, self).clean()
-		password = self.cleaned_data.get('password')
-		confirm_password = self.cleaned_data.get('confirm_password')
-
-		if password != confirm_password:
-			self._errors['password'] = self.error_class(['Passwords do not match. Try again'])
-		return self.cleaned_data
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
 class PostPictureForm(forms.ModelForm):
     class Meta:
