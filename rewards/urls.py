241ac4ab5b04161819a8378f968c3a2cbec48521
zizoubrown <zizumoha2015@gmail.com>
2020-10-28 11:39:32 +0300

    modify urls

--- rewards/urls.py
+++ rewards/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+    path(r'upload/', views.upload, name='upload'),
+    path(r'search/', views.search_results, name='search_results')
 ]
